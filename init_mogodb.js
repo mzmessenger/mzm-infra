e6bde727a1ed50aab29a953341168c789dfbb112
kohta ito <kohta110@gmail.com>
2019-06-10 22:53:22 +0900

    nginx config

--- init_mogodb.js
+++ init_mogodb.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 /**
- * ./init_mogodb.js --password=example --user=xxx --user_password=yyy
+ * ./init_mogodb.js --password=example --user=xxx --user_password=yyy --port=27018
  */
 
 const { execSync } = require('child_process')
-const { password, user, user_password } = require('yargs')
+const { password, user, user_password, port } = require('yargs')
   .option('password', {
     describe: 'root password',
     demandOption: true
@@ -17,17 +17,22 @@ const { password, user, user_password } = require('yargs')
   .option('user_password', {
     describe: 'db user password',
     demandOption: true
+  })
+  .option('port', {
+    describe: 'db port',
+    demandOption: true,
+    default: '27017'
   }).argv
 
-createUser('mzm')
-createUser('auth')
+function createMongoShell(dbname) {
+  return `mongo ${dbname} --port '${port}' -uroot -p ${password} --authenticationDatabase="admin"`
+}
 
 function createUser(dbname) {
   let existUser = true
+  const mongoShell = createMongoShell(dbname)
   try {
-    execSync(
-      `mongo ${dbname} -uroot -p ${password} --authenticationDatabase="admin" --eval 'db.getUsers()' | grep user | grep ${user}`
-    )
+    execSync(`${mongoShell} --eval 'db.getUsers()' | grep user | grep ${user}`)
   } catch (e) {
     if (e.stderr.toString() === '') {
       existUser = false
@@ -37,24 +42,24 @@ function createUser(dbname) {
   try {
     if (!existUser) {
       execSync(
-        `mongo ${dbname} -uroot -p ${password} --authenticationDatabase="admin" --eval 'db.createUser({ user: "${user}", pwd: "${user_password}", roles: [] })'`
+        `${mongoShell} --eval 'db.createUser({ user: "${user}", pwd: "${user_password}", roles: [] })'`
       )
     }
     execSync(
-      `mongo ${dbname} -uroot -p ${password} --authenticationDatabase="admin" --eval 'db.grantRolesToUser("${user}", ["readWrite"])'`
+      `${mongoShell} --eval 'db.grantRolesToUser("${user}", ["readWrite"])'`
     )
   } catch (e) {
     console.error(`${e.toString()}\n\n${e.stdout.toString()}`)
   }
 }
 
+createUser('mzm')
+createUser('auth')
+
+const mzmShell = createMongoShell('mzm')
 // index
 execSync(
-  `mongo mzm -uroot -p ${password} --authenticationDatabase="admin" --eval 'db.rooms.createIndex({ name: 1 }, { unique: true })'`
+  `${mzmShell} --eval 'db.rooms.createIndex({ name: 1 }, { unique: true })'`
 )
-execSync(
-  `mongo mzm -uroot -p ${password} --authenticationDatabase="admin" --eval 'db.enter.createIndex({ userId: 1 })'`
-)
-execSync(
-  `mongo mzm -uroot -p ${password} --authenticationDatabase="admin" --eval 'db.users.createIndex({ account: 1 })'`
-)
+execSync(`${mzmShell} --eval 'db.enter.createIndex({ userId: 1 })'`)
+execSync(`${mzmShell} --eval 'db.users.createIndex({ account: 1 })'`)
